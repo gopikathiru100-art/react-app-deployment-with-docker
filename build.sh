f757d3aacca4398d6d4e3996eab47a25e4bb646b
gopikathiru100-art <gopikathiru100@gmail.com>
2025-09-30 15:15:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 # Exit immediately if a command fails
 set -e
 
+# Variables
+IMAGE_NAME=react-ci-cd
+DOCKER_REPO=$DOCKER_USERNAME/$IMAGE_NAME
+
 # Login to Docker Hub securely
 echo $DOCKER_PASS | docker login -u $DOCKER_USERNAME --password-stdin
 
@@ -10,8 +14,11 @@ echo $DOCKER_PASS | docker login -u $DOCKER_USERNAME --password-stdin
 docker stop react || true
 docker rm react || true
 
-# Enable BuildKit for modern builds
-DOCKER_BUILDKIT=1 docker build -t react-ci/cd .
+# Enable BuildKit for modern builds and build image
+DOCKER_BUILDKIT=1 docker build -t $DOCKER_REPO:latest .
 
-# Run container on port 80
-docker run -d --name react -p 80:80 react-ci/cd
+# Push image to Docker Hub
+docker push $DOCKER_REPO:latest
+
+# Run container locally on Jenkins server
+docker run -d --name react -p 80:80 $DOCKER_REPO:latest
