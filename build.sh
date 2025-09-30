912d6eb48b6d6c69cc97b970c06df65b01324f5d
gopikathiru100-art <gopikathiru100@gmail.com>
2025-09-30 15:12:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-#login into DockerHub:
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
+# Exit immediately if a command fails
+set -e
 
-#stopping existing container:
-docker stop react
-docker rm react
+# Login to Docker Hub securely
+echo $DOCKER_PASS | docker login -u $DOCKER_USERNAME --password-stdin
 
-#building a image:
-docker build -t react-ci/cd .
+# Stop and remove old container if exists
+docker stop react || true
+docker rm react || true
 
-#running a container from the created image:
-docker run -d -it --name react -p 80:80 react-ci/cd
+# Enable BuildKit for modern builds
+DOCKER_BUILDKIT=1 docker build -t react-ci/cd .
 
-#pushing the image to dockerhub:
-docker tag react-ci/cd gopikathiru100/react-app:ci-cd
-docker push gopikathiru100/react-app:ci-cd
+# Run container on port 80
+docker run -d --name react -p 80:80 react-ci/cd
